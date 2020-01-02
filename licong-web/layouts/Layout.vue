4120233f16e09b06b5740a5704357ddcfc3af1b3
LiC0ng <a1040484@gmail.com>
2020-01-02 18:50:56 +0900

    feat: 调整页面布局
    - 重命名了布局文件
    - 调整了页面的布局

---
+++ licong-web/layouts/Layout.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="app-wapper">
+    <el-container>
+      <el-aside width="200px">Aside</el-aside>
+      <el-container>
+        <el-header>Header</el-header>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+
+<style>
+  .app-wapper {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
+
+  .el-header, .el-footer {
+   background-color: #B3C0D1;
+   color: #333;
+   text-align: center;
+   line-height: 60px;
+  }
+
+  .el-aside {
+    background-color: #D3DCE6;
+    color: #333;
+    text-align: center;
+    line-height: 200px;
+  }
+
+  .el-main {
+    background-color: #E9EEF3;
+    color: #333;
+    text-align: center;
+    line-height: 160px;
+  }
+
+  .el-container {
+    margin-bottom: 40px;
+    height: 100%;
+  }
+</style>
