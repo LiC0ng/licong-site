e03236206591e478bcc0349f57bccf148085d7e3
LiC0ng <a1040484@gmail.com>
2020-01-22 20:32:57 +0900

    feat(gateway): 添加了网关模块
    - 使用zuul网关控制访问

---
+++ licong-server/licong_web/src/main/java/co/licong/web/filter/WebFilter.java
@@ -0,0 +1,48 @@
+package co.licong.web.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Component
+public class WebFilter extends ZuulFilter {
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        //得到request上下文
+        RequestContext requestContext = RequestContext.getCurrentContext();
+        //得到request域
+        HttpServletRequest request = requestContext.getRequest();
+        //得到头信息
+        String header = request.getHeader(HttpHeaders.AUTHORIZATION);
+        // 放行zuul的第一次请求
+        if (request.getMethod().equals("OPTIONS")){
+            return null;
+        }
+        //判断是否有头信息
+        if (header != null && !"".equals(header)) {
+            //把头信息继续向下传
+            System.out.println(header);
+            requestContext.addZuulRequestHeader("Authorization", header);
+        }
+        return null;
+    }
+}
